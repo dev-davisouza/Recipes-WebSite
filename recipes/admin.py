a258b9012b5217e6b3db07dde9e358325ac313e3
Davi Souza <vlogsplayer10@gmail.com>
2023-07-06 11:53:51 -0300

    Model Admin & MEDIA path

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
