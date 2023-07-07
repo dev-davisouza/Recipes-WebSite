b162a9d792259131e59766ee560a1e41b52c0be8
Davi Souza <vlogsplayer10@gmail.com>
2023-07-07 19:37:17 -0300

    filter by is_published

--- recipes/views.py
+++ recipes/views.py
@@ -5,13 +5,13 @@ from .models import Recipe
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id)
+        category__id=category_id, is_published=True)
     context = {'recipes': recipes, }
-    return render(request, 'recipes/pages/home.html', context)
+    return render(request, 'recipes/pages/category.html', context)
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('updated_at')
+    recipes = Recipe.objects.filter(is_published=True).order_by('updated_at')
     context = {'recipes': recipes, }
     return render(request, 'recipes/pages/home.html', context)
 
