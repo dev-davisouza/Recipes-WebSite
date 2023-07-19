ea8f53e9d35696aa640fdc20de78b46ebe099e3b
Davi Souza <vlogsplayer10@gmail.com>
2023-07-19 07:44:21 -0300

    Testing Assertions

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        i = '123'
-        print('olá mundo')
-        assert 1 == 1
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home')  # Testing the home URL
+        self.assertEqual(home_url, '/')
