edf12b4f55375196832fbc5a3d0a14f44dd9f278
Davi Souza <vlogsplayer10@gmail.com>
2023-07-19 12:48:59 -0300

    Testing if the urls resolve to their functions

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from . import views
 
 
 class RecipeURLsTest(TestCase):
@@ -14,3 +15,21 @@ class RecipeURLsTest(TestCase):
     def test_recipe_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_view_is_correct(self):
+        view = resolve(
+            reverse('recipes:category', args=(1,))
+        )
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_detail_view_is_correct(self):
+        view = resolve(
+            reverse('recipes:recipe', args=(1,))
+        )
+        self.assertIs(view.func, views.recipe)
