222c0d820454a38f85ef64e1e99e5902df66624d
Davi Souza <vlogsplayer10@gmail.com>
2023-07-19 08:32:41 -0300

    more tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -6,3 +6,11 @@ class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
         home_url = reverse('recipes:home')  # Testing the home URL
         self.assertEqual(home_url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', args=(1,))
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
