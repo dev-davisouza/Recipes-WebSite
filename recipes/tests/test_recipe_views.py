ecf0e0aa76a28e8a5299045a8a40de46559620c4
Davi Souza <vlogsplayer10@gmail.com>
2023-07-22 22:26:04 -0300

    Tests for Category

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -20,7 +20,7 @@ class RecipeViewsTest(TestCase):
         self.client.force_login(self.user)
 
         # Create a Category object test
-        self.category = models.Category.objects.create(name='Asiáticas')
+        self.category = models.Category.objects.create(name='Asiáticas', id=1)
 
         # Create an Recipe object with the attr is_published=True
         self.recipe = models.Recipe.objects.create(
@@ -39,10 +39,29 @@ class RecipeViewsTest(TestCase):
             author=self.user
         )
 
+    # -------------------- Home Section --------------------
+
     def test_recipe_home_view_is_correct(self):
         # Getting the view by the URL
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
+
+    def test_recipe_home_view_returns_status_200_OK(self):
+        # Getting the HTTP response object by the URL
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_returns_status_404_Not_Found(self):
+        # Getting the HTTP response object by the URL
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        # Getting the HTTP response object by the URL
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    # -------------------- Category Section --------------------
 
     def test_recipe_category_view_is_correct(self):
         # Getting the view by the URL with args
@@ -51,19 +70,26 @@ class RecipeViewsTest(TestCase):
         )
         self.assertIs(view.func, views.category)
 
+    def test_recipe_category_view_returns_status_200_OK(self):
+        # Getting the HTTP response object by the URL
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_category_view_returns_status_404_Not_Found(self):
+        # Getting the HTTP response object by the URL
+        response = self.client.get(reverse('recipes:category', args=(2,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_category_view_loads_correct_template(self):
+        # Getting the HTTP response object by the URL
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        self.assertTemplateUsed(response, 'recipes/pages/category.html')
+
+    # -------------------- Recipe Section --------------------
+
     def test_recipe_detail_view_is_correct(self):
         # Getting the view by the URL with kwargs
         view = resolve(
             reverse('recipes:recipe', kwargs={'id': 1})
         )
         self.assertIs(view.func, views.recipe)
-
-    def test_recipe_home_view_returns_status_200_OK(self):
-        # Getting the HTTP response object by the URL
-        response = self.client.get(reverse('recipes:home'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_recipe_home_view_loads_correct_template(self):
-        # Getting the HTTP response object by the URL
-        response = self.client.get(reverse('recipes:home'))
-        self.assertTemplateUsed(response, 'recipes/pages/home.html')
