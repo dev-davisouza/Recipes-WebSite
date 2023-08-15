9312945be22dead94b1f7e25b16cfe25b0ef2951
Davi Souza <vlogsplayer10@gmail.com>
2023-08-15 11:09:35 -0300

    correcting some inconsistences in the project

--- project/settings.py
+++ project/settings.py
@@ -122,9 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'base_static',
-]
 
 STATIC_ROOT = BASE_DIR / 'static'
 
